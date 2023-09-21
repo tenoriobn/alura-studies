008c20d1c4c7884f7e124c883e12dccd94ff00cb
Bruno Tenório <tenoriobn@gmail.com>
2023-09-21 11:30:07 -0300

    Estilizando com Sass

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './style.scss';
 
 function Lista() {
 
@@ -14,11 +15,11 @@ function Lista() {
   }];
 
   return (
-    <aside>
+    <aside className='listaTarefas'>
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index}>
+          <li key={index} className='item'>
             <h3>{item.tarefa}</h3>
             <span>{item.tempo}</span>
           </li>
