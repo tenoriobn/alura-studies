6c7a947772df71f7b142762d2493c7cce570c1bf
Bruno Tenório <tenoriobn@gmail.com>
2023-09-30 15:32:05 -0300

    Selecionando uma tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,10 +1,35 @@
 import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
-  console.log('item atual:', tarefa, tempo, selecionado, completado, id)
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) =>void
+}
+
+export default function Item(
+  {
+    tarefa, 
+    tempo, 
+    selecionado, 
+    completado, 
+    id, 
+    selecionaTarefa
+  }: Props
+) 
+
+{
   return (
-    <li className={style.item}>
+    <li 
+      className={style.item} 
+      onClick={() => selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+      )}
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,11 +7,20 @@ import { ITarefa } from '../types/tarefa';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
 
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+  }
+
+  console.log(tarefas)
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista 
+        tarefas={tarefas} 
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
